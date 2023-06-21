88e833904aebd59b0029e1cb6edd997d7980d1f9
vincealwynsibay1 <vincealwynsibay1@gmail.com>
2023-06-22 07:44:48 +0800

    fix bugs

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -2,26 +2,22 @@ import { useEffect, useRef } from "react";
 import Card from "./Card";
 
 function Grid({ cards, handleClick }) {
-  const grid = useRef();
+  let grid = [];
+  let group = [];
+  for (let i = 0; i < cards.length; i++) {
+    group.push(cards[i]);
 
-  useEffect(() => {
-    grid.current = [];
-    let group = [];
-    for (let i = 0; i < cards.length; i++) {
-      group.push(cards[i]);
-
-      // cards length needs to be a perfect square
-      if (i > 0 && (i + 1) % Math.sqrt(cards.length) == 0) {
-        grid.current.push(group);
-        group = [];
-      }
+    // cards length needs to be a perfect square
+    if (i > 0 && (i + 1) % Math.sqrt(cards.length) == 0) {
+      grid.current.push(group);
+      group = [];
     }
-  }, []);
+  }
 
   return (
     <div className="grid">
       {grid &&
-        grid.current.map((group, idx) => {
+        grid.map((group, idx) => {
           return (
             <div className="grid-group" key={cards.length * idx}>
               {group.map((card, idx2) => {
